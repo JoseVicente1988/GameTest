0dcbd47f76398dc21270c53977c5469274285e28
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-06-24 00:47:09 +0200

    Use enemy texture instead of scene color

--- scripts/resources/enemy_data.gd
+++ scripts/resources/enemy_data.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name EnemyData
 
 @export var enemy_name: String = "Enemy"
-@export var scene_color: Color = Color(1.0, 0.2, 0.2)
+@export var enemy_texture: Texture2D
 @export var base_health: int = 100
 @export var base_damage: int = 10
 @export var base_armor: int = 2
@@ -17,12 +17,12 @@ func build_stats(floor_number: int, player_power: float) -> Dictionary:
     var player_scale: float = 1.0 + (player_power * player_power_weight / 100.0)
     return {
         "name": enemy_name,
+        "texture": enemy_texture,
         "health": int(base_health * floor_scale * player_scale),
         "max_health": int(base_health * floor_scale * player_scale),
         "damage": int(base_damage * floor_scale * player_scale),
         "armor": int(base_armor * floor_scale),
         "xp": int(base_xp * floor_scale),
         "gold": int(base_gold * floor_scale),
-        "color": scene_color,
         "drops": drops
     }
