72cb4c8e0e5bcdec6001be988644752bd41b4110
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-06-24 00:23:52 +0200

    Add ad rewards and expanded permanent base upgrades

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,6 +18,8 @@ var permanent_damage_level: int = 0
 var permanent_armor_level: int = 0
 var permanent_health_level: int = 0
 var permanent_gold_level: int = 0
+var permanent_all_stats_level: int = 0
+var ad_training_level: int = 0
 
 var max_health: int = 180
 var health: int = 180
@@ -39,10 +41,18 @@ func reset_run() -> void:
     xp = 0
     xp_to_next = 25
     run_gold = 0
-    max_health = 180 + permanent_health_level * 25
+
+    var all_health_bonus: int = permanent_all_stats_level * 15
+    var all_damage_bonus: int = permanent_all_stats_level * 2
+    var all_armor_bonus: int = permanent_all_stats_level * 1
+
+    var ad_health_bonus: int = ad_training_level * 8
+    var ad_damage_bonus: int = ad_training_level * 1
+
+    max_health = 180 + permanent_health_level * 25 + all_health_bonus + ad_health_bonus
     health = max_health
-    damage = 18 + permanent_damage_level * 4
-    armor = 5 + permanent_armor_level * 2
+    damage = 18 + permanent_damage_level * 4 + all_damage_bonus + ad_damage_bonus
+    armor = 5 + permanent_armor_level * 2 + all_armor_bonus
     regen = 0
     enemy_damage_reduction = 0
     enemy_armor_reduction = 0
@@ -71,13 +81,38 @@ func add_xp(amount: int) -> void:
         leveled_up.emit()
     stats_changed.emit()
 
-func add_gold(amount: int) -> void:
+func add_gold(amount: int) -> int:
     var final_amount: int = int(float(amount) * get_gold_multiplier())
     final_amount = max(1, final_amount)
     run_gold += final_amount
     persistent_gold += final_amount
     save_progress()
     stats_changed.emit()
+    meta_changed.emit()
+    return final_amount
+
+func add_ad_gold(amount: int) -> int:
+    var final_amount: int = max(1, amount)
+    run_gold += final_amount
+    persistent_gold += final_amount
+    save_progress()
+    stats_changed.emit()
+    meta_changed.emit()
+    return final_amount
+
+func double_current_run_gold_from_ad() -> int:
+    var bonus: int = max(1, run_gold)
+    persistent_gold += bonus
+    run_gold += bonus
+    save_progress()
+    stats_changed.emit()
+    meta_changed.emit()
+    return bonus
+
+func add_ad_training_level() -> void:
+    ad_training_level += 1
+    save_progress()
+    reset_run()
     meta_changed.emit()
 
 func get_gold_multiplier() -> float:
@@ -88,7 +123,8 @@ func apply_item(item: ItemData) -> void:
     armor += item.armor_bonus
     max_health += item.max_health_bonus
     health += item.max_health_bonus
-    add_gold(item.gold_bonus)
+    if item.gold_bonus > 0:
+        add_gold(item.gold_bonus)
     stats_changed.emit()
 
 func apply_upgrade(upgrade: UpgradeData, times_picked: int) -> void:
@@ -117,6 +153,8 @@ func get_upgrade_cost(upgrade_id: String) -> int:
         return 70 + permanent_health_level * 50
     elif upgrade_id == "gold":
         return 90 + permanent_gold_level * 70
+    elif upgrade_id == "all_stats":
+        return 140 + permanent_all_stats_level * 95
     return 999999
 
 func buy_permanent_upgrade(upgrade_id: String) -> bool:
@@ -134,6 +172,8 @@ func buy_permanent_upgrade(upgrade_id: String) -> bool:
         permanent_health_level += 1
     elif upgrade_id == "gold":
         permanent_gold_level += 1
+    elif upgrade_id == "all_stats":
+        permanent_all_stats_level += 1
     else:
         persistent_gold += cost
         return false
@@ -143,6 +183,16 @@ func buy_permanent_upgrade(upgrade_id: String) -> bool:
     meta_changed.emit()
     return true
 
+func get_base_stats_text() -> String:
+    return "Base DMG Lv.%s | ARM Lv.%s | HP Lv.%s | Gold Lv.%s | All Lv.%s | Ad Training Lv.%s" % [
+        permanent_damage_level,
+        permanent_armor_level,
+        permanent_health_level,
+        permanent_gold_level,
+        permanent_all_stats_level,
+        ad_training_level
+    ]
+
 func save_progress() -> void:
     var config: ConfigFile = ConfigFile.new()
     config.set_value("currency", "persistent_gold", persistent_gold)
@@ -150,6 +200,8 @@ func save_progress() -> void:
     config.set_value("upgrades", "armor", permanent_armor_level)
     config.set_value("upgrades", "health", permanent_health_level)
     config.set_value("upgrades", "gold", permanent_gold_level)
+    config.set_value("upgrades", "all_stats", permanent_all_stats_level)
+    config.set_value("ads", "training", ad_training_level)
     config.save(SAVE_PATH)
 
 func load_progress() -> void:
@@ -163,6 +215,8 @@ func load_progress() -> void:
     permanent_armor_level = int(config.get_value("upgrades", "armor", 0))
     permanent_health_level = int(config.get_value("upgrades", "health", 0))
     permanent_gold_level = int(config.get_value("upgrades", "gold", 0))
+    permanent_all_stats_level = int(config.get_value("upgrades", "all_stats", 0))
+    ad_training_level = int(config.get_value("ads", "training", 0))
     meta_changed.emit()
 
 func reset_save() -> void:
@@ -171,5 +225,7 @@ func reset_save() -> void:
     permanent_armor_level = 0
     permanent_health_level = 0
     permanent_gold_level = 0
+    permanent_all_stats_level = 0
+    ad_training_level = 0
     save_progress()
     reset_run()
