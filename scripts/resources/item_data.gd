1bd4ee2469483116a65613605602b5263f088935
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-06-24 00:09:22 +0200

    Add item resource script

---
+++ scripts/resources/item_data.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name ItemData
+
+@export var item_name: String = "Item"
+@export var description: String = ""
+@export var drop_chance: float = 0.25
+@export var damage_bonus: int = 0
+@export var armor_bonus: int = 0
+@export var max_health_bonus: int = 0
+@export var gold_bonus: int = 0
