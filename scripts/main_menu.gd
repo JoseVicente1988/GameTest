e5a8d459dd164a36e17ffbfbdf3a747dfbe30ccc
Jose Vicente Vicedo Silla <josevicentevicedosilla@gmail.com>
2026-06-24 00:13:37 +0200

    Point menu to working auto game scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_start_button_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/Game.tscn")
+    get_tree().change_scene_to_file("res://scenes/GameAuto.tscn")
